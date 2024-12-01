43a6b92f8481cbcfd92e39b120ed07d93b280812
Daniel Meint <danimeint@gmail.com>
2024-12-01 16:25:34 +0100

    Refactor input processing and calculations w/ chatgpt

--- aoc-24/01/sol.py
+++ aoc-24/01/sol.py
@@ -1,43 +1,16 @@
+# Read input
+with open("input.txt") as f:
+    left, right = zip(*(map(int, line.split()) for line in f))
+
+# Part 1: Calculate total distance
+sorted_left = sorted(left)
+sorted_right = sorted(right)
+total_distance = sum(abs(a - b) for a, b in zip(sorted_left, sorted_right))
+print(f"Part 1: Total Distance = {total_distance}")
+
+# Part 2: Calculate similarity score
 from collections import Counter
 
-list1, list2 = [], []
-
-
-def get_lines_as_list(file):
-    with open(file) as f:
-        return f.read().splitlines()
-
-
-for line in get_lines_as_list('input.txt'):
-    elem1, elem2 = line.split()
-    list1.append(int(elem1))
-    list2.append(int(elem2))
-
-assert len(list1) == len(list2)
-
-n = len(list1)
-
-list1.sort()
-list2.sort()
-
-sol1 = 0
-
-for i in range(n):
-    abs_diff = abs(list1[i] - list2[i])
-    sol1 += abs_diff
-
-# print(res) # p1 sol
-
-# part2
-sol2 = 0
-
-counter2 = Counter(list2)
-
-print(counter2)
-
-for elem1 in list1:
-    print(elem1)
-    sol2 += elem1 * counter2[elem1]
-
-print('sol2', sol2)
-
+right_count = Counter(right)
+similarity_score = sum(num * right_count[num] for num in left)
+print(f"Part 2: Similarity Score = {similarity_score}")
